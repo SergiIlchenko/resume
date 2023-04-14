ae3f94e9be3f2bb00e24c7b8182fbc2191702c6f
SergiIlchenko <sergi.ilchenko@gmail.com>
2023-04-14 21:07:38 +0300

    урок 3

--- src/route/index.js
+++ src/route/index.js
@@ -1498,6 +1498,16 @@ router.get('/task31', function (req, res) {
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
+
+router.get('/bootstrap3', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap3', {
+    layout: 'bootstrap',
+  })
+})
+
 // ================================================================
 // Підключаємо роутер до бек-енду
 module.exports = router
